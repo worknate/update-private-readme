305b03ef24e5944ab8c8c63cd228f9e179547034
Nate Stilwell <nstilwell@lextegrity.com>
2021-03-17 10:43:50 -0400

    Stub in everything

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
+<!--
+  WARNING: this file is auto-generated!
+  Make changes to readme.mustache
+-->
+
 # update-private-readme
+
+[![Version badge](https://img.shields.io/badge/version-1.0.0-blue.svg)](https://shields.io/)
+
+## Manually update readme
+
+```npm run readme```
+
---
+++ index.js
@@ -0,0 +1,18 @@
+const Mustache = require('mustache');
+const fs = require('fs');
+const MUSTACHE_MAIN_DIR = './readme.mustache';
+
+let DATA = {
+    version: process.env.npm_package_version ?? 'unknown'
+};
+
+function generateReadMe() {
+    fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
+        if (err) throw err;
+        const output = "<!--\n  WARNING: this file is auto-generated!\n  Make changes to readme.mustache\n-->\n\n" + Mustache.render(data.toString(), DATA);
+        fs.writeFileSync('README.md', output);
+
+        console.log('process.env', process.env)
+    });
+}
+generateReadMe();
